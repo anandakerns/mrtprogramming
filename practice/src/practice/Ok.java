130a901260055059064f7149360f074e1dcf39a0
anandak <anandak@192.168.1.13>
2017-10-02 20:09:09 -0700

    practice

---
+++ practice/src/practice/Ok.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Ok {
+	//class 
+	public static void main(String[] args) {
+		//main method- shows where to start running a application
+		System.out.println("hello world");
+		System.out.print("practice");
+		//statements- will show text when run
+	}
+}
